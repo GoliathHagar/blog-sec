2ec24473b81d1f0cc83c60226b6b0d0cbbc329a7
Junior Monteiro <junior.monteiro@powersolution.cv>
2023-03-21 10:23:03 +0000

    backend: server side app initialized

---
+++ src/backend/blog-sec/src/main/java/com/msi/blogsec/BlogSecApplication.java
@@ -0,0 +1,13 @@
+package com.msi.blogsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSecApplication.class, args);
+	}
+
+}
---
+++ src/backend/blog-sec/src/test/java/com/msi/blogsec/BlogSecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msi.blogsec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
