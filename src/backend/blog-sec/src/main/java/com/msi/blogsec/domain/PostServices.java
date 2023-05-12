a97b58ffb504ca70407e9de166ccfec2cdc66d26
Junior Monteiro <junior.monteiro@powersolution.cv>
2023-05-12 09:53:39 +0100

    backend: implementation initial state

---
+++ src/backend/blog-sec/src/main/java/com/msi/blogsec/domain/PostServices.java
@@ -0,0 +1,15 @@
+package com.msi.blogsec.domain;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author : goliathhagar
+ * @CreatedBy : IntelliJ IDEA
+ * @ProjectName : blog-sec
+ * @mailto : goliathhagar@outlook.com
+ * @created : 5/12/23, Friday, 9:51 AM
+ **/
+
+@Service
+public class PostServices {
+}
