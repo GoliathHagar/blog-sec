d131aca4aaa3c7a50b2f3338e36a6670f1150df4
Junior Monteiro <junior.monteiro@powersolution.cv>
2023-05-24 19:31:35 +0100

    backend: updates

--- src/backend/blog-sec/src/test/java/com/msi/blogsec/domain/PostServicesTest.java
+++ src/backend/blog-sec/src/test/java/com/msi/blogsec/domain/PostServicesTest.java
@@ -1,6 +1,7 @@
 package com.msi.blogsec.domain;
 
 import com.msi.blogsec.api.controllers.models.input.PostInputModel;
+import com.msi.blogsec.api.security.helpers.SecurityHelper;
 import com.msi.blogsec.data.Post;
 import com.msi.blogsec.domain.constants.PostStatus;
 import com.msi.blogsec.domain.models.PostSearchParams;
@@ -9,9 +10,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
+
+import static org.mockito.Mockito.when;
 
 /**
  * @author : goliathhagar
@@ -27,12 +31,19 @@ class PostServicesTest {
     @Autowired
     PostServices postServices;
 
+    @MockBean
+    SecurityHelper securityHelper;
+
     @BeforeEach
     void setUp() {
     }
 
     @Test
     void getPosts() {
+        when(securityHelper.jwtSubId()).thenReturn("user-id");
+        when(securityHelper.getclaims("name")).thenReturn("user name");
+        when(securityHelper.getclaims("email")).thenReturn("user@name.com");
+
         postServices.createPost(new PostInputModel("test",
                 "content", PostStatus.PUBLISHED, true, "tag"));
 
